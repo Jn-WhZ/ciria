c0bd32b42ba9645efb5757bf59053483bf610b03
Jn-WhZ <118285789+Jn-WhZ@users.noreply.github.com>
2025-11-17 11:47:51 +0100

    Implement CORS middleware in main.py
    Add CORS middleware to allow cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.routers import test
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # à restreindre plus tard
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI(
     title="CIR Backend API",
     description="Backend utilisé par Bubble.io pour le projet CIR",
